94f6d86963adbfa02876c5b2c652867c36326fc6
camilo <acde100@gmail.com>
2025-03-04 17:23:14 -0500

    second commit adding css files

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const template = document.getElementById('card-template');
   const clone = template.content.cloneNode(true);
-  clone.querySelector('.title').textContent = "Título dinámico";
-  document.getElementById('clone-block').appendChild(clone);
+  clone.querySelector('.title').textContent = "TITULOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO";
+  clone.querySelector('.content').textContent = "Deseas salir de la app";
+  document.getElementById('clone-block2').appendChild(clone);
